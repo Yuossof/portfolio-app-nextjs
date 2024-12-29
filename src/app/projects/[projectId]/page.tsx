3234ef4fe529d8a2e2d2fa59e409453f74c2a17c
Youssof <youssofwr@gmail.com>
2024-12-30 00:46:56 +0200

    dd1

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -6,7 +6,7 @@ import { Github, Globe } from 'lucide-react';
 const Page = async ({ params }: { params: Promise<{ projectId: string }> }) => {
   const projectId = (await params).projectId
 
-    const response = await fetch(`/api/admin/projects/${projectId}`, {
+    const response = await fetch(`/api/projects/${projectId}`, {
       next: {
         revalidate: 120
       },
